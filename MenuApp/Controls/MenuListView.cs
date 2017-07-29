ecf88eb49f907ef91d520ec2f6f8a24161036e9e
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-07-29 09:07:43 -0500

    MenuListView

---
+++ MenuApp/Controls/MenuListView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MenuApp.Pages;
+using Xamarin.Forms;
+
+namespace MenuApp.Controls
+{
+    public class MenuListView : ListView
+    {
+    }
+}
