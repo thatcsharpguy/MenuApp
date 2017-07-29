b9f6e46c72cbfd807f57693e6f1fc813575f7040
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-07-29 09:11:40 -0500

    Add custom menu listview

--- MenuApp/Controls/MenuListView.cs
+++ MenuApp/Controls/MenuListView.cs
@@ -10,5 +10,32 @@ namespace MenuApp.Controls
 {
     public class MenuListView : ListView
     {
+        public MenuListView()
+        {
+            // TODO: Setup menus
+            var menus = new List<MenuListItem>
+            {
+                new MenuListItem
+                {
+                    Title = "Gimnasios",
+                    Icon = "gym.png",
+                    Page = typeof(GymsPage)
+                },
+                new MenuListItem
+                {
+                    Title = "Elite 4",
+                    Icon = "elite.png",
+                    Page = typeof(EliteFourPage)
+                },
+                new MenuListItem
+                {
+                    Title = "Tipos pokemon",
+                    Icon = "types.png",
+                    Page = typeof(PokemonTypesPage)
+                }
+            };
+
+            ItemsSource = menus;
+        }
     }
 }
