5a205ee4774baed043b61f30236c7e328870c28e
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-07-29 08:59:54 -0500

    MenuListItem class.

---
+++ MenuApp/Controls/MenuListItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuApp.Controls
+{
+    public class MenuListItem
+    {
+        public string Title { get; set; }
+        public string Icon { get; set; }
+        public Type Page { get; set; }
+    }
+}
