ec034e544d978484827395bf83d0150140621c6e
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-07-28 22:26:37 -0500

    Main page (in csproj) and Default pages.

--- MenuApp/Pages/MainPage.cs
+++ MenuApp/Pages/MainPage.cs
@@ -9,5 +9,31 @@ namespace MenuApp.Pages
 {
     public class MainPage : MasterDetailPage
     {
+        public MainPage()
+        {
+            Master = new ContentPage
+            {
+                Title= "Master page",
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        new Label{ Text = "Master page" }
+                    }
+                }
+            };
+
+            Detail = new ContentPage
+            {
+                Title = "Detail page",
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        new Label{ Text = "Detail page" }
+                    }
+                }
+            };
+        }
     }
 }
