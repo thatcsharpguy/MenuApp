75387204e0080764fed1a78c57b6c8f0e0972d25
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-07-29 09:31:07 -0500

    MenuListItemCell

---
+++ MenuApp/Controls/MenuListItemCell.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MenuApp.Controls
+{
+    public class MenuListItemCell : ImageCell
+    {
+    }
+}
