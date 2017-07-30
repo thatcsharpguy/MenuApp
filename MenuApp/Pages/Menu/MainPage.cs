d716fcf025554c9066c605aa4c5a50562c8734b4
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-07-29 19:19:27 -0500

    Reuse pages.

--- MenuApp/Pages/Menu/MainPage.cs
+++ MenuApp/Pages/Menu/MainPage.cs
@@ -9,16 +9,21 @@ namespace MenuApp.Pages.Menu
 {
     public class MainPage : MasterDetailPage
     {
+        private Dictionary<Type, MenuNavigationPage> _pages;
+
         public MainPage()
         {
+            _pages = new Dictionary<Type, MenuNavigationPage>();
+
             var sideMenuPage = new SideMenuPage();
             sideMenuPage.MenuItemSelected += SideMenuPage_MenuItemSelected;
             Master = sideMenuPage;
 
-
             var elite4Page = new MenuNavigationPage(new EliteFourPage());
             elite4Page.InitialPageAppearing += Page_InitialPageAppearing;
             elite4Page.InitialPageDisappearing += Page_InitialPageDisappearing;
+
+            _pages.Add(typeof(EliteFourPage), elite4Page);
 
             Detail = elite4Page;
         }
@@ -26,10 +31,14 @@ namespace MenuApp.Pages.Menu
         private void SideMenuPage_MenuItemSelected(object sender, Controls.MenuListItem e)
         {
             MenuNavigationPage page;
-            var actualContentPage = (Page)Activator.CreateInstance(e.Page);
-            page = new MenuNavigationPage(actualContentPage);
-            page.InitialPageAppearing += Page_InitialPageAppearing;
-            page.InitialPageDisappearing += Page_InitialPageDisappearing;
+            if (!_pages.TryGetValue(e.Page, out page))
+            {
+                var actualContentPage = (Page)Activator.CreateInstance(e.Page);
+                page = new MenuNavigationPage(actualContentPage);
+                page.InitialPageAppearing += Page_InitialPageAppearing;
+                page.InitialPageDisappearing += Page_InitialPageDisappearing;
+                _pages.Add(e.Page, page);
+            }
             Detail = page;
             IsPresented = false;
         }
